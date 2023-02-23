85adf7617fbd78e450ab7b8c4ea6b121fc1c398f
Simon Horrabin <simonhorrabin@gmail.com>
2023-02-23 18:23:48 -0500

    linked the js

--- script.js
+++ script.js
@@ -14,31 +14,32 @@ const questionText = document.getElementById("question-text")
 let currentQuestion = 0;
 let score = 0;
 
-let questions = [    {        question: "What is my dog's name?",        answers: [            {option: "Bella", answer: true},            {option: "Ziggy", answer: false},        ]
-    },
-    {
-        question: "My favorite crayon is the ___ one.",
-        answers: [
-            {option: "Yellow", answer: false},
-            {option: "Purple", answer: true},
-       ]
-    },
-    {
-        question: "The capital of North Carolina is ...",
-        answers: [
-            {option: "Raleigh", answer: true},
-            {option: "Asheville", answer: false},
-            {option: "Charlotte", answer: false},
-        ]
-    }
+let questions = [{
+    question: "What is my dog's name?", answers: [{ option: "Bella", answer: true }, { option: "Ziggy", answer: false },]
+},
+{
+    question: "My favorite crayon is the ___ one.",
+    answers: [
+        { option: "Yellow", answer: false },
+        { option: "Purple", answer: true },
+    ]
+},
+{
+    question: "The capital of North Carolina is ...",
+    answers: [
+        { option: "Raleigh", answer: true },
+        { option: "Asheville", answer: false },
+        { option: "Charlotte", answer: false },
+    ]
+}
 ]
 
 // Add events buttons
 
 restartBtn.addEventListener("click", restart);
-prevBtn.addEventListener("click", prev);
-nextBtn.addEventListener("click",next);
-submitBtn.addEventListener("click",submit);
+// prevBtn.addEventListener("click", prev);
+nextBtn.addEventListener("click", next);
+// submitBtn.addEventListener("click", submit);
 
 // Start the quiz
 function beginQuiz() {
@@ -47,38 +48,38 @@ function beginQuiz() {
     questionText.innerHTML = questions[currentQuestion].question;
     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
     trueBtn.onclick = () => {
-        if(questions[currentQuestion].answers[0].answer) {
-            if(score < 3) {
+        if (questions[currentQuestion].answers[0].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion < 2) {
+        if (currentQuestion < 2) {
             next();
         }
     }
- 
+
     falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
     falseBtn.onclick = () => {
-        if(questions[currentQuestion].answers[1].answer) {
-            if(score < 3) {
+        if (questions[currentQuestion].answers[1].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion < 2) {
+        if (currentQuestion < 2) {
             next();
         }
     };
- 
+
     prevBtn.classList.add("hide");
- }
- 
- beginQuiz();
+}
 
- // restart button
+beginQuiz();
 
- function restart() {
+// restart button
+
+function restart() {
     currentQuestion = 0;
     prevBtn.classList.remove("hide");
     nextBtn.classList.remove("hide");
@@ -88,32 +89,32 @@ function beginQuiz() {
     score = 0;
     userScore.innerHTML = score;
     beginQuiz();
- }
+}
 
- //
+//
 
- function next() {
+function next() {
     currentQuestion++;
-    if(currentQuestion >= 2) {
+    if (currentQuestion >= 2) {
         nextBtn.classList.add("hide");
         prevBtn.classList.remove("hide");
     }
     questionText.innerHTML = questions[currentQuestion].question;
     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
     trueBtn.onclick = () => {
-        if(questions[currentQuestion].answers[0].answer) {
-            if(score < 3) {
+        if (questions[currentQuestion].answers[0].answer) {
+            if (score < 3) {
                 score++;
             }
         }
         userScore.innerHTML = score;
-        if(currentQuestion < 2) {
+        if (currentQuestion < 2) {
             next();
         }
     }
- 
+
     falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
     falseBtn.onclick = () => {
-        if(questions[currentQuestion].answers[1].answer);
+        if (questions[currentQuestion].answers[1].answer);
     }
-        
+}
